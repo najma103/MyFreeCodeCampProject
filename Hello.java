81c8f97c95a7aaad74710adf977d2cf37ce18980
Najma Aden <najma.aden@gmail.com>
2016-04-24 17:44:44 -0400

    Initial Java "Hello World" project

---
+++ Hello.java
@@ -0,0 +1,10 @@
+  class Hello {
+     public static void main(String[] args) {
+		 int count = 2;
+		 if (count >= 3) {
+            System.out.println("hello world");
+		} else {
+			System.out.println("Testing textmate haha");
+		}
+     }
+  }
---
+++ StudentTest.java
@@ -0,0 +1,3 @@
+  public class StudentTest extends junit.framework.TestCase {
+     
+  }
